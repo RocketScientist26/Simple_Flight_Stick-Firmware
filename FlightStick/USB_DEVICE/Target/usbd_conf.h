cec8d1655b4a66fe0caa9dc89e3a11aa803afff9
Rocket-Scientist <info@rocket-scientist.me>
2022-08-21 18:17:37 +0300

    Brightness slider output added in descriptor

--- FlightStick/USB_DEVICE/Target/usbd_conf.h
+++ FlightStick/USB_DEVICE/Target/usbd_conf.h
@@ -75,7 +75,7 @@
 /*---------- -----------*/
 #define USBD_CUSTOMHID_OUTREPORT_BUF_SIZE     64
 /*---------- -----------*/
-#define USBD_CUSTOM_HID_REPORT_DESC_SIZE     56
+#define USBD_CUSTOM_HID_REPORT_DESC_SIZE     67
 /*---------- -----------*/
 #define CUSTOM_HID_FS_BINTERVAL     1
 
